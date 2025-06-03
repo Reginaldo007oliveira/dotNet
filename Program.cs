5980687ef86b0d5d99ee2ec35abff93b46cead69
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-06-03 14:02:35 -0300

    comando for

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int numero = 5;
-for (int contador = 0; contador <= 9; contador++){
-    Console.WriteLine($"{numero} x {numero * 1}");
+int numero = 75;
+for (int contador = 0; contador <= 10 ; contador++){
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
 }
--- obj/Debug/net8.0/visualnoise.AssemblyInfo.cs
+++ obj/Debug/net8.0/visualnoise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("visualnoise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14304ad4e5a3216d3efd8e7fa63361dc1587ad81")]
 [assembly: System.Reflection.AssemblyProductAttribute("visualnoise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("visualnoise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
