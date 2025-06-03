14304ad4e5a3216d3efd8e7fa63361dc1587ad81
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-06-03 13:50:10 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numero = 5;
+for (int contador = 0; contador <= 9; contador++){
+    Console.WriteLine($"{numero} x {numero * 1}");
+}
