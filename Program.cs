176468b938e5e9285dda089e91a385ae838e41fb
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-06-03 14:05:23 -0300

    loop de repetição for

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-// See https://aka.ms/new-console-template for more information
+ // See https://aka.ms/new-console-template for more information
+
+// Print a greeting message to the console
 Console.WriteLine("Hello, World!");
 
+// Declare an integer variable 'numero' and assign it the value 75
 int numero = 75;
+
+// Loop from 0 to 10 inclusive using the variable 'contador'
 for (int contador = 0; contador <= 10 ; contador++){
+    // Print the multiplication of 'numero' by 'contador' in a formatted string
     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
 }
