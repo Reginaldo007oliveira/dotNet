84c3577b1843fdde75de41d136a81d613d5a3d84
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-06-04 14:53:15 -0300

    While com a contagem modificada

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@ Console.WriteLine("Hello, World!");
 
 // Declare an integer variable 'numero' and assign it the value 75
 int numero = 75;
-int contador = 0;
+int contador = 1;
 
 // Loop from 0 to 10 inclusive using the variable 'contador'
 while (contador <= 10)
 {
     // Print the multiplication of 'numero' by 'contador' in a formatted string
-    Console.WriteLine($"{contador + 1}º  Execuções {numero} * {contador} = {numero * contador}");
+    Console.WriteLine($"{contador}º  Execuções {numero} * {contador} = {numero * contador}");
      // se não colocar o contador no while o programa entra em loop infinito
     contador++;
 }
